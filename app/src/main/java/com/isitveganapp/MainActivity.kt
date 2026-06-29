67012600f38bd960de73026762fc9a52b964d913
Dennis Hong <dehong@ebay.com>
2026-06-29 14:08:10 -0700

    portrait on phone

--- app/src/main/java/com/isitveganapp/MainActivity.kt
+++ app/src/main/java/com/isitveganapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.isitveganapp
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +13,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Lock phones to portrait. smallestScreenWidthDp is orientation-independent
+        // (always the short axis), so sw600dp reliably identifies tablets/foldables.
+        val isWideScreen = resources.configuration.smallestScreenWidthDp >= 600
+        if (!isWideScreen) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+
         enableEdgeToEdge()
         setContent {
             IsItVeganTheme {
